4a57180831dea65f7d46116db8543e72bd0aa363
Shiby-Mathew <shiby.ajay@gmail.com>
2023-08-30 12:48:37 +1000

    added about me section

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,17 +1,52 @@
-
 import React from "react";
 import "./skills.css";
 
-
-
-const Skills=()=>{
-    return(
-        <section id="skills">
-            
-About section
-
-        </section>
-
-    )
-}
+const Skills = () => {
+  return (
+    <section id="skills">
+      <span className="skillTitle">About Me</span>
+      <span className="skillDesc">
+        A junior full stack developer with a proven ability to adapt in both
+        self-starting and collaborative environments while staying focused on
+        achieving high-quality results under strict deadlines. Eager to obtain a
+        challenging position that will expand my learning and build upon my
+        developer skills.
+      </span>
+      <div className="skillBars">
+        <div className="skillBar">
+          <img src="" alt="" className="skillBarImg" />
+          <div className="skillBarText">
+            <h2>Front End Skills</h2>
+            <p>
+              Knowledge in HTML, CSS, and JavaScript, the core languages of
+              front-end development.Strong understanding of responsive web
+              design principles and the ability to create mobile-friendly
+              layouts. Knowledge of JavaScript front-end library React.
+            </p>
+          </div>
+        </div>
+        <div className="skillBar">
+          <img src="" alt="" className="skillBarImg" />
+          <div className="skillBarText">
+            <h2>Back End Skills</h2>
+            <p>
+              Knowledge in Node.js, Express.js, GraphQL,APIs the core languages
+              of Back end development.
+            </p>
+          </div>
+        </div>
+        <div className="skillBar">
+          <img src="" alt="" className="skillBarImg" />
+          <div className="skillBarText">
+            <h2>Databases</h2>
+            <p>Good Knowledge in MySQL and MonogoDB(Mongoose ORM)</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 export default Skills;
+{
+  /* <span className=""></span> */
+}
